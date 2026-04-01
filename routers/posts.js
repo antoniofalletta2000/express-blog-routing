dfcc206e25e3d3fbdad46eeda995bcb3c3ce6a10
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-04-01 12:47:03 +0200

    complete bonus

--- routers/posts.js
+++ routers/posts.js
@@ -45,7 +45,12 @@ router.get("/", (req, res) => {
 })
 //show
 router.get("/:id", (req,res)=>{
-    res.send(`Udapte post with id ${req.params.id}`)
+    const post= posts.find(post=>post.id===parseInt(req.params.id))
+    if(post){
+        res.json(post)
+    }else{
+        res.status(404).json({message:"Post not found"})
+    }
 })
 //store
 router.post("/", (req,res)=>{
